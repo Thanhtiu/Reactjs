183802f521a32f084e789cfb3ad70a8bf5af0455
tranvanchilinh1003 <chilinh140220@gmail.com>
2024-07-22 20:32:17 +0700

    fix lỗi nhỏ

--- src/routes/admin/router.js
+++ src/routes/admin/router.js
@@ -3,19 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import Header from '../../layouts/admin/component/Header';
 import Home from '../../pages/admin/home/Home';
 import Footer from '../../layouts/admin/component/Footer';
-import InsertCate from '../../pages/admin/caterories/InsertCate';
 import InsertCustomers from '../../pages/admin/customers/InsertCustomers';
 import EditCustomer from '../../pages/admin/customers/EditCustomer';
 import CustomerList from '../../pages/admin/customers/CustomerList';
 import Menu from '../../layouts/admin/component/Menu';
+import InsertCate from '../../pages/admin/categories/InsertCate';
+import ListCate from '../../pages/admin/categories/ListCate';
 // import ListCate from '../../pages/admin/categories/ListCate';
 import '../../assets/admin/styles/vendor/fontawesome-free/css/all.min.css';
 import '../../assets/admin/styles/vendor/bootstrap/css/bootstrap.min.css';
 import '../../assets/admin/styles/css/ruang-admin.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import InsertCate from '../../pages/admin/categories/InsertCate';
-import ListCate from '../../pages/admin/categories/ListCate';
+
 
 const AppRouterAdmin = () => {
   return (
